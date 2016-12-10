b7f18bf99d5dc0e0ebf37f327973672b12e418eb
Alexey Shiklomanov <ashiklom@bu.edu>
2016-12-10 09:51:32 -0800

    Cleanup of Python script
    Add some metadata, including name and email.
    
    Also, removed Jupyter comments, and added some whitespace for
    readability.

--- processed/gc_gene_plot.py
+++ processed/gc_gene_plot.py
@@ -1,36 +1,27 @@
 
-# coding: utf-8
-
-# In[1]:
+# Python script for plotting gene data
+# Author: Alexey Shiklomanov
+# Email: alexey.shiklomanov@gmail.com
 
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 
-# %matplotlib inline
-
-
-# In[2]:
-
 filename = sys.argv[1]
+
 # Usage:
 # python gc_gene_plot.py myfile
 # filename = "myfile"
+
 print("You are analyzing file:")
 print(filename)
 
 human_chr21 = pd.read_table(filename, sep ='\t')
 human_chr21
 
-
-# In[13]:
-
 human_chr21['gc_content'] = human_chr21['gc_bases'] /    (human_chr21['win_end'] - human_chr21['win_start'])
 
 human_chr21['gene_content'] = human_chr21['exon_bases'] /    (human_chr21['win_end'] - human_chr21['win_start'])
-
-
-# In[11]:
 
 plt.plot(human_chr21['gc_content'], human_chr21['gene_content'], 'o')
 plt.xlabel('GC content')
